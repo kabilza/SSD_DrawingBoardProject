ddb4bfb145b208f26416bfc35c38f909960d691a
bleachjade <bleachjade@hotmail.com>
2020-04-10 21:49:22 +0700

    recalculate region fixed

--- src/objects/CompositeGObject.java
+++ src/objects/CompositeGObject.java
@@ -37,33 +37,30 @@ public class CompositeGObject extends GObject {
 		}
 	}
 
-
 	public void recalculateRegion() {
 		int newX = gObjects.get(0).x;
 		int newY = gObjects.get(0).y;
-		int newDx = gObjects.get(0).x + gObjects.get(0).width;
-		int newDy = gObjects.get(0).y + gObjects.get(0).height;
-		for (GObject gObject : this.gObjects) {
+		int newWidth = gObjects.get(0).x + gObjects.get(0).width;
+		int newHeight = gObjects.get(0).y + gObjects.get(0).height;
+		for (GObject gObject : gObjects) {
 			if (gObject.x < newX) {
 				newX = gObject.x;
 			}
-			if (gObject.x + gObject.width > newDx) {
-				newDx = gObject.x + gObject.width;
+			if (gObject.x + gObject.width > newWidth) {
+				newWidth = gObject.x + gObject.width;
 			}
 			if (gObject.y < newY) {
 				newY = gObject.y;
 			}
-			if (gObject.y + gObject.height > newDy) {
-				newDy = gObject.y + gObject.height;
+			if (gObject.y + gObject.height > newHeight) {
+				newHeight = gObject.y + gObject.height;
 			}
 		}
 		this.x = newX;
 		this.y = newY;
-		this.width = newDx - newX;
-		this.height = newDy - newY;
+		width = newWidth - newX;
+		height = newHeight - newY;
 	}
-
-	
 
 
 	@Override
@@ -78,7 +75,7 @@ public class CompositeGObject extends GObject {
 	public void paintLabel(Graphics g) {
 		// TODO: Implement this method.
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.drawString("Group all",x,y);
+		g2d.drawString("Group all",x,y-5);
 	}
 	
 }
